d38054ca625073f0a54fb243b84b3d19ea173486
isamidori <isabela_rodrigues97@hotmail.com>
2022-11-13 16:25:43 -0300

    inserinfo funcao d leitura

--- Afuga/src/com/algoritimos/senacafuga/Jogo.java
+++ Afuga/src/com/algoritimos/senacafuga/Jogo.java
@@ -13,75 +13,79 @@ public class Jogo {
 	public static void main(String[] args) throws Exception {
 //		String resposta, ver, escolha;
 //		List<String> bolsa = new ArrayList<String>();
-		String userRobson = "\\robso\\workspace\\";
-		Arquivo.lerArquivo("C:\\Users" + userRobson + "Senac-AFuga\\Afuga\\src\\falas\\hitoriaInicial\\historiaInicial.txt");
-		
-//		FileReader arq = new FileReader("C:\\Users\\robso\\workspace\\Senac-AFuga\\Afuga\\falas\\historiaInicial");
-//		BufferedReader lerArq = new BufferedReader(arq);
-//	    System.out.println(lerArq);
-		//Arquivo.lerArquivo("C:\\Users\\robso\\workspace\\Senac-AFuga\\Afuga\\falas\\HistoriaInicial");
-//		Instanciando a classe Personagens
-//		Salvar um arquivo com os personagens
-		
-//		Personagem pedro = new Personagem();
-//		pedro.setNome("Pedro");
-//		pedro.setClasse("Assassino");
-//		pedro.setPontoForte("Habilidades com faca");
-//		pedro.setForca(3);
-//		
-//		Personagem thiago = new Personagem();
-//		pedro.setNome("Thiago");
-//		pedro.setClasse("Mágico");
-//		pedro.setPontoForte(""); //decidir
-//		pedro.setForca(3);
-//		
-//		Personagem matheus = new Personagem();
-//		pedro.setNome("Matheus");
-//		pedro.setClasse("Guerreiro");
-//		pedro.setPontoForte("Atirador");
-//		pedro.setForca(3);
-//		
-//		
-//		DesafioUm.desafioUm("Pedro");
-//		
-		
-		
-		
-		
-//		/* Inicio do Jogo */
-//		resposta = Historia.historiaInicial();
-//		SalvarArquivo.gerarArquivo(resposta);
-//		System.out.println("História gravada no arquivo");
-//		
-//		System.out.println(resposta);
-//		
-//		
-//		/* Descrição e escolha dos personagens */	
-//		resposta = Personagem.descricao();
-//		System.out.println(resposta);
-//
-//		System.out.println("Escolha a sua classe: ");
-//		String personagem = input.next().toUpperCase();
-//		
-//		resposta = Personagem.classe(personagem);
-//		System.out.println(resposta);
-//				
-//		ver = input.next().toUpperCase();
-//		bolsa = Bolsa.bolsa(personagem, ver, null);
-//		System.out.println(bolsa);
 
 
-//		
-//		
-//		DesafioUm desafioUm = new DesafioUm();
-//		
-//		resposta = Historia.historiaDesafioUm();
-//		System.out.println(resposta);
-//		
-//		desafioUm.parteUm();
-//		desafioUm.parteDois(personagem);
-//		desafioUm.parteTres();
-//                
+String userIsabela = "\\isabe\\OneDrive\\Documentos\\";
+		
+
+		Arquivo.lerArquivo("C:\\Users" + userIsabela + "Senac-AFuga\\Afuga\\falas\\Desafio1\\texto2.txt");
+		
+// 		FileReader arq = new FileReader("C:\\Users\\robso\\workspace\\Senac-AFuga\\Afuga\\falas\\historiaInicial");
+// 		BufferedReader lerArq = new BufferedReader(arq);
+// 	    System.out.println(lerArq);
+// 		Arquivo.lerArquivo("C:\\Users\\robso\\workspace\\Senac-AFuga\\Afuga\\falas\\HistoriaInicial");
+// 		Instanciando a classe Personagens
+// 		Salvar um arquivo com os personagens
+		
+// 		Personagem pedro = new Personagem();
+// 		pedro.setNome("Pedro");
+// 		pedro.setClasse("Assassino");
+// 		pedro.setPontoForte("Habilidades com faca");
+// 		pedro.setForca(3);
+		
+// 		Personagem thiago = new Personagem();
+// 		pedro.setNome("Thiago");
+// 		pedro.setClasse("Mágico");
+// 		pedro.setPontoForte(""); //decidir
+// 		pedro.setForca(3);
+		
+// 		Personagem matheus = new Personagem();
+// 		pedro.setNome("Matheus");
+// 		pedro.setClasse("Guerreiro");
+// 		pedro.setPontoForte("Atirador");
+// 		pedro.setForca(3);
+		
+		
+// 		DesafioUm.desafioUm("Pedro");
+		
+		
+		
+		
+		
+// 		/* Inicio do Jogo */
+// 		resposta = Historia.historiaInicial();
+// 		SalvarArquivo.gerarArquivo(resposta);
+// 		System.out.println("História gravada no arquivo");
+		
+// 		System.out.println(resposta);
+		
+		
+// 		/* Descrição e escolha dos personagens */	
+// 		resposta = Personagem.descricao();
+// 		System.out.println(resposta);
+
+// 		System.out.println("Escolha a sua classe: ");
+// 		String personagem = input.next().toUpperCase();
+		
+// 		resposta = Personagem.classe(personagem);
+// 		System.out.println(resposta);
+				
+// 		ver = input.next().toUpperCase();
+// 		bolsa = Bolsa.bolsa(personagem, ver, null);
+// 		System.out.println(bolsa);
+
+
+// //		
+// //		
+// 		DesafioUm desafioUm = new DesafioUm();
+		
+// 		resposta = Historia.historiaDesafioUm();
+// 		System.out.println(resposta);
+		
+// 		desafioUm.parteUm();
+// 		desafioUm.parteDois(personagem);
+// 		desafioUm.parteTres();
+// //                
 //                DesafioDois desafioDois = new DesafioDois();
 //                
 //                resposta = Historia.historiaDesafioDois();
