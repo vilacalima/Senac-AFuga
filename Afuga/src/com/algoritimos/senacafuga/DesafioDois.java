5826904ea4025acdcac4e3c1e7d3b132352360b9
Nathalia <cerqueiran86@gmail.com>
2022-11-16 20:31:51 -0300

    subindo alterações desafio dois

--- Afuga/src/com/algoritimos/senacafuga/DesafioDois.java
+++ Afuga/src/com/algoritimos/senacafuga/DesafioDois.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class DesafioDois {
 
 	private static Scanner input = new Scanner(System.in);
-	public static void desafioDois(String personagem) {
+        private static String userAcer = "\\acer\\Desktop\\";
+	public static void desafioDois(String personagem) throws Exception {
 		String opcoes;
 		int num = 0;
 		boolean menu = false;
@@ -14,24 +15,20 @@ public class DesafioDois {
                          
 			switch (opcoes) {
 			case "DICA":
-                      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\OpcaoDica4.txt");
-
-                            
+                      
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\OpcaoDica4.txt");
 				num = input.nextInt();    
 				break;
                                 
 			case "NAO":
-		      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\OpcaoNao5.txt");
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\OpcaoNao5.txt");
 				num = input.nextInt();
 				break;
 
 			case "AJUDA":
-		      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\OpcaoAjuda3.txt");
-                      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\OpcaoAjudapartedois.txt");
-                      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\OpcaoAjudapartetres.txt");
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\OpcaoAjuda3.txt");
+                      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\OpcaoAjudapartedois.txt");
+                      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\OpcaoAjudapartetres.txt");
 
 				num = input.nextInt();
 				break;
@@ -39,14 +36,12 @@ public class DesafioDois {
 
 			switch (num) {
 			case 133:
-		      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\RespostaCase1.txt");
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\RespostaCase1.txt");
 				menu = true;
 				acertou(personagem);
 				break;
 			default:
-                      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\RespostaCase2.txt");				
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\RespostaCase2.txt");				
                       break;
                       
 			}
@@ -54,30 +49,25 @@ public class DesafioDois {
 		} while (!menu);
 	}
 	
-	public static void acertou(String personagem) {
+	public static void acertou(String personagem) throws Exception {
 		String ver, desatar;
 
 		do {
 
-                      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\SegundaparteIntroducao6.txt");					+ "Para desatar esse n�, pegue o canivete que est� na sua mochila e corte o n�. \n");
-			ver = input.next().toUpperCase();
+                    Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\SegundaparteIntroducao6.txt");					
                         
                        
 			if (ver.equals("VER")) {
 				Bolsa.bolsa(personagem, ver, null);
-		     String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\SegundaparteVer7.txt");					+ "Para desatar esse n�, pegue o canivete que est� na sua mochila e corte o n�. \n");
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\SegundaParteVer7.txt");					
 			}
 		} while (ver.equals("null"));
 		 
 		do {
-		      String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\SegundaparteDesatar8.txt");	
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\SegundaparteDesatar8.txt");	
 			desatar = input.next().toUpperCase();
 			if (desatar.equals("DESATAR")) {
-                       String userAcer = "\\acer\\Documentos\\";
-		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Documents\DesafioDois\Terceiraparte9.txt");			
+		      Arquivo.lerArquivo("C:\\Users" + userAcer + "Senac-AFuga\\Afuga\\src\\falas\\DesafioDois\\Terceiraparte9.txt");			
                         }
 
 		} while (!desatar.equals("DESATAR"));               
